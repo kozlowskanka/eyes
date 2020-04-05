ff70618ac348cc6010cd66d64bd6f04c512b2e4d
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-05 18:32:50 +0200

    Setting modules

--- app/script.js
+++ app/script.js
@@ -1,25 +1,95 @@
 import React from 'react';
 import { render } from 'react-dom';
+import AppDescription from './../components/AppDescription/AppDescription';
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
-        <button className="btn btn-close">X</button>
-      </div>
-    )
+  state = {
+    status: "off",
+    time: 0,
+    timer: null,
   }
-};
+
+  formatTime = time => {
+
+    let formatedTime;
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+    
+    if(minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    if(seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    formatedTime = minutes + ' : ' + seconds;
+
+    return formatedTime;
+  };
+
+  step = () => {
+    this.setState({time: this.state.time - 1});
+
+    if (this.state.time === 0) {
+      this.playBell();
+
+      if (this.state.status === 'work') {
+        this.setState({
+          status: 'rest',
+          time: 20,
+        })
+      } else if (this.state.status === 'rest') {
+        this.setState({
+          status: 'work',
+          time: 1200,
+        });
+      };
+    }
+  };
+
+  startTimer = () => {
+    this.setState({
+      status: 'work',
+      time: 1200,
+      timer: setInterval(this.step, 1000),
+    });
+  };
+
+  stopTimer = () => {
+    this.setState({
+      status: 'off',
+      time: 0,
+      timer: clearInterval(this.state.timer),
+    });
+  };
+
+  closeApp = () => {
+    window.close();
+  }
+
+  playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  };
+
+  render() {
+      
+    const { status, time } = this.state;
+      return (
+        <div>
+          <h1>Protect your eyes</h1>
+          {status === 'off' && <AppDescription/>}
+          {(status === 'work') && <img src="./images/work.png" />}
+          {(status === 'rest') && <img src="./images/rest.png" />}
+          {(status !== 'off') && <div className="timer">{this.formatTime(time)}</div>}
+          {(status === 'off') && <button className="btn" onClick={() => this.startTimer()}>Start</button>}
+          {(status !== 'off') && <button className="btn">Stop</button>}
+          <button className="btn btn-close" onClick={() => this.closeApp()}>X</button>
+        </div>
+      )
+    };
+  }
 
 render(<App />, document.querySelector('#app'));
