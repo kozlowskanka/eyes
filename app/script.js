e1d6d164c4ecdffd7bba3f4fe48842a58ad7af01
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-05 20:08:54 +0200

    Add gitignore

--- app/script.js
+++ app/script.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import AppDescription from './../components/AppDescription/AppDescription';
+
+const AppDescription = () => (
+  <div>
+    <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+    <p>This app will help you track your time and inform you when it's time to rest.</p>
+  </div>
+);
 
 class App extends React.Component {
 
@@ -25,7 +31,7 @@ class App extends React.Component {
     }
 
     formatedTime = minutes + ' : ' + seconds;
-
+    
     return formatedTime;
   };
 
