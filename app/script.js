455f69c80b85be4bc55fee95f4498c3293bdc4e7
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-06 15:22:26 +0200

    Fix lack of gitignore file

--- app/script.js
+++ app/script.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const AppDescription = () => (
-  <div>
-    <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-    <p>This app will help you track your time and inform you when it's time to rest.</p>
-  </div>
-);
+import AppDescription from './components/AppDescription/AppDescription';
 
 class App extends React.Component {
 
